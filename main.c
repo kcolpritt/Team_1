506f9f1b38c91031d8dca764f72b2363acc9f37e
kcolpritt <kcolpritt@wpi.edu>
2017-01-22 23:11:59 -0500

    Update main.c
    This is the code for the serial. It should compile but the only thing wrong might be the way i named the registers. I added a "1" inside but not sure if that's correct.

--- main.c
+++ main.c
@@ -8,8 +8,65 @@
 #include "main.h"
 
 
+/*
+ * main.c
+ *
+ *  Created on: Sep 14, 2016
+ *      Author: Kenneth
+ */
 
-int main(){
+#include <RBELib/RBELib.h> //RBELib
+#include <avr/io.h>
+
+unsigned int aDelay;
+
+
+#define BAUD 9600
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1) // calculation to set baudrate in the registers
+
+//this function initializes UART
+void UART_init (void)
+{
+	UBRR1H = (BAUDRATE>>8);	//shits the register to the right by 8 bits which splits up UBRR
+	UBRR1L = BAUDRATE;
+	UCSR1B |= (1<<TXEN)|(1<<RXEN);  //sets the receiving and transmitting bits  to one so they are enabled
+	UCSR1C |= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);  //8 bit data format
+}
+
+void uart_transmit (unsigned char data)
+{
+	while (1(UCSR1A & (1<<UDRE))); // wait while the register is free
+	UDR = data;  //upload data to register
+}
+
+unsigned char uart_receive (void)
+{
+	while(!(UCSR1A) & (1<<RXC));
+	return UDR;
+}
+
+
+int main (void)
+{
+	unsigned char data_sr;
+
+	UART_init();
+
+	while(1)
+	{
+		data_sr = uart_receive();
+		_delay_ms(100);
+	}
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
